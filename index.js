08dff2119c243e001da540a48c912bfb33c0dfcf
Luiz Zamboni <luizamboni2002@hotmail.com>
2018-08-04 16:03:31 -0300

    ADD events routes

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ const express = require("express")
 const app = express()
 
 
+const events = [
+
+]
+
 app.get("/api/v1/health", (req, res) => {
 
   res.json({ ok: "ok" })
+})
+
+
+app.post("/api/v1/events", (req, res) => {
+  const { body: attrs } = req
+  events.push(attrs)
+
+  res.json({ acknowledge: true })
+})
+
+
+app.get("/api/v1/events", (req, res) => {
+
+  events.push(events)
 })
 
 app.listen(8080, () => {
