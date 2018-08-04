bd234808d8cbf0099ed033cadc32c578e69e21d3
Luiz Zamboni <luizamboni2002@hotmail.com>
2018-08-04 15:49:19 -0300

    Fix PORT

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/api/v1/health", (req, res) => {
   res.json({ ok: "ok" })
 })
 
-app.listen(3000, () => {
-  console.log("server started at port 3000")
+app.listen(8080, () => {
+  console.log("server started at port 8080")
 })
